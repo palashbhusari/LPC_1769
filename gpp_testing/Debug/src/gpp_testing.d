cb594a3176839e7f9015d772d8b36683ac08bf0a
Palash Bhusari <31458267+palashbhusari@users.noreply.github.com>
2022-05-03 22:13:16 -0700

    GPIO testing
    Testing the GPIO of LPC1769

---
+++ gpp_testing/Debug/src/gpp_testing.d
@@ -0,0 +1,11 @@
+src/gpp_testing.o src/gpp_testing.d: ../src/gpp_testing.c \
+ /Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/LPC17xx.h \
+ /Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/core_cm3.h \
+ /Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/core_cmInstr.h \
+ /Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/core_cmFunc.h \
+ /Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/system_LPC17xx.h
+/Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/LPC17xx.h:
+/Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/core_cm3.h:
+/Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/core_cmInstr.h:
+/Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/core_cmFunc.h:
+/Users/palash/Documents/MCUXpressoIDE_11.4.0_6237/workspace/CMSIS_CORE_LPC17xx/inc/system_LPC17xx.h:
